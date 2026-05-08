a4c23811fea61dfe6786ae9ee6e6d827e5287aea
JJYNG1023 <j.yeung2020@outlook.com>
2026-05-08 21:08:42 +0000

    add comments model

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,11 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.foreignkey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.foreignkey(User, on_delete=models.CASCADE, related_name="blog_comments")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
